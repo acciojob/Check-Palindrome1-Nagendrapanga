ece2437f60cbf84169e13d5acaebff6ce1a03310
Nagendrapanga <116372316+Nagendrapanga@users.noreply.github.com>
2023-03-24 14:45:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
+	var start=str(0);
+	var end=str.length-1;
+	while(start<=end){
+		if(str.charAt(start)===str.chartAt(end)){
+			start++;
+			end--
+		}
+		else{
+			return false;
+		}
+	}
+	return true;
 
 }
 module.exports = palindrome
